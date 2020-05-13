89f14acc82ff425dcfe9e6a3edcbd60ea92e53b1
Alex180299 <ale_18.99@hotmail.com>
2020-05-13 10:02:08 -0500

    First commit

--- src/main/java/com/api/inscriptions/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.inscriptions.repository;
-
-import com.api.inscriptions.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
